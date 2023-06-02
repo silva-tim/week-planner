104bc1f8ee453a2b78f8d5921375b5dac86cf099
David Kyong <dvdkyong@gmail.com>
2023-06-02 18:04:09 +0000

    david's work teehe.

---
+++ data.js
@@ -0,0 +1,11 @@
+let todos = [];
+
+const previousTodosJSON = localStorage.getItem(‘javascript-local-storage’);
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+function handleUnload(event) {
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem(‘javascript-local-storage’, todosJSON);
+}
+window.addEventListener(‘beforeunload’, handleUnload);
--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+let todos = [];
+
+const previousTodosJSON = localStorage.getItem(‘javascript-local-storage’);
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+function handleUnload(event) {
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem(‘javascript-local-storage’, todosJSON);
+}
+window.addEventListener(‘beforeunload’, handleUnload);
+
+
+
+
 const $background = document.querySelector('.hidden');
 const $openModal = document.querySelector('.open-modal');
 const $closeModal = document.querySelector('.close-modal');
@@ -12,3 +27,17 @@ function closeButton(event) {
 
 $openModal.addEventListener('click', openButton);
 $closeModal.addEventListener('click', closeButton);
+
+const $form = document.querySelector('form');
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const todo = {
+    day: $form.elements.day.value,
+    time: $form.elements.time.value,
+    description: $form.elements.description.value
+  };
+
+  todos.push(todo);
+
+});
